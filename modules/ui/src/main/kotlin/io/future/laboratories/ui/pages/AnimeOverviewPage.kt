2bc662a3a250c997191c7e4222a59a6427a911a8
Speedy_242 <dbw29229@gmail.com>
2023-10-26 13:18:02 +0200

    Implement custom Saver for tags

--- modules/ui/src/main/kotlin/io/future/laboratories/ui/pages/AnimeOverviewPage.kt
+++ modules/ui/src/main/kotlin/io/future/laboratories/ui/pages/AnimeOverviewPage.kt
@@ -1,5 +1,6 @@
 package io.future.laboratories.ui.pages
 
+import android.annotation.SuppressLint
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
@@ -20,9 +21,12 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.Saver
+import androidx.compose.runtime.saveable.SaverScope
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
+import androidx.compose.runtime.snapshots.SnapshotStateList
+import androidx.compose.runtime.toMutableStateList
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import io.future.laboratories.anilistapi.data.MediaList
@@ -60,8 +64,19 @@ public fun AnimeOverviewPage(
     var tagSearchActive by rememberSaveable { mutableStateOf(false) }
 
     // TagFilter
-    // TODO: Custom Saver
-    val selectedTags = remember { mutableStateListOf<MediaTag>() }
+    val saver = object : Saver<SnapshotStateList<MediaTag>, String> {
+        private val separator = ", "
+        override fun restore(value: String): SnapshotStateList<MediaTag> {
+            return value.split(separator).map { MediaTag(name = it) }.toMutableStateList()
+        }
+
+        override fun SaverScope.save(value: SnapshotStateList<MediaTag>): String {
+            return value.joinToString(separator = separator) { it.name }
+        }
+    }
+
+    @SuppressLint("MutableCollectionMutableState")
+    val selectedTags by rememberSaveable(stateSaver = saver) { mutableStateOf(mutableStateListOf()) }
 
     DefaultSearchBar(
         query = animeQuery,
