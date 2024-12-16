44e004acd2f3cb5b1c37ed04c9f9d1ccd272fb17
Speedy_242 <dbw29229@gmail.com>
2024-12-16 16:25:15 +0100

    feat(ui): Add load more button to Anime Browser Page
    Added a "Load More" button to the Anime Browser Page to allow users to fetch more anime.
    Also added German translations for the new string.

--- modules/ui/src/main/kotlin/io/future/laboratories/ui/pages/AnimeBrowserPage.kt
+++ modules/ui/src/main/kotlin/io/future/laboratories/ui/pages/AnimeBrowserPage.kt
@@ -1,21 +1,27 @@
 package io.future.laboratories.ui.pages
 
+import android.R.attr.onClick
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.SideEffect
 import androidx.compose.runtime.snapshots.SnapshotStateList
 import androidx.compose.runtime.snapshots.SnapshotStateMap
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import io.future.laboratories.anilistapi.data.Media
 import io.future.laboratories.anilistapi.data.PageQueryParams
 import io.future.laboratories.ui.Constants
+import io.future.laboratories.ui.R
 import io.future.laboratories.ui.components.AnimeBrowserItem
 import io.future.laboratories.ui.components.BrowserSearchbar
+import io.future.laboratories.ui.components.PositiveButton
 
 @OptIn(ExperimentalFoundationApi::class)
 @Composable
@@ -57,5 +63,20 @@ public fun AnimeBrowserPage(
                 )
             }
         }
+
+        if(pages.isNotEmpty()) {
+            item(key="load-more") {
+                PositiveButton(
+                    modifier = Modifier.fillMaxWidth(),
+                    onClick = {
+                        currentQueryParams.pageNumber.value = pages.size + 1
+                        onRequestMore(currentQueryParams.copy())
+                    }
+                ) {
+                    Text(text = stringResource(id = R.string.load_more))
+                }
+            }
+        }
+
     }
 }
